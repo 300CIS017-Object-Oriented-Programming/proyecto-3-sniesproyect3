af7ef8c0f2a05afc40372ed017437e98371aa2ed
isabella erazo <isabella23146@gmail.com>
2024-11-10 23:18:16 -0500

    ADD: main.py

--- src/Settings.py
+++ src/Settings.py
@@ -3,4 +3,4 @@ class Settings:
     BASE_PATH = "C:/SNIES_EXTRACTOR/inputs/"
     OUTPUTS_PATH = "C:/SNIES_EXTRACTOR/outputs/"
     DELIMITADOR = ';'
-    PROGRAM_NAME_COLUMN = 'program_name'  # Set default column name for program name search
+    PROGRAM_NAME_COLUMN = 'PROGRAMA ACADÉMICO'  # Set default column name for program name search
